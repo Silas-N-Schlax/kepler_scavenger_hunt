795dca273b9f47f8472d20983732553f2d4fd4ed
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-05-05 21:21:28 -0400

    created header animations on phone for hamburger menu

---
+++ src/assets/js/header.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("open");
+    navLinks.classList.toggle("open");
+  });
+});
