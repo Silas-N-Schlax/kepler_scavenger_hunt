81f7e9965e198ffb7a574e1b71e754ff3f5973cb
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-05-05 21:17:01 -0400

    basic mongo db connection

---
+++ netlify/functions/utils/database.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require('mongodb');
+
+class Database {
+  constructor() {
+    this.db = null;
+    this.client = null;
+  }
+
+  async connectToDatabase() {
+    if (this.db) {
+      // Return the existing connection if already established
+      return this.db;
+    }
+
+    try {
+      const url = process.env.MONGODB_URI;
+      const dbName = "KeplerGames";
+
+      // Create a new MongoClient instance and connect
+      this.client = new MongoClient(url);
+      await this.client.connect();
+      console.log("Connected to MongoDB!!");
+
+      // Set the database instance
+      this.db = this.client.db(dbName);
+      return this.db;
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+      return null;
+    }
+  }
+
+  async returnDatabase() {
+    // Ensure the database connection is established before returning it
+    if (!this.db) {
+      await this.connectToDatabase();
+    }
+    return this.db;
+  }
+
+  async closeConnection() {
+    if (this.client) {
+      await this.client.close();
+      console.log("MongoDB connection closed.");
+      this.db = null;
+      this.client = null;
+    }
+  }
+}
+
+// Export an instance of the Database class
+module.exports = new Database();
