0a7006b766ee64042b3c19ad37596b1701f94ef1
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-05-08 07:02:06 -0400

    created script to create team profiles

---
+++ src/assets/js/meet-the-team.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", async function () {
+  const response = await fetch("/.netlify/functions/getAllUserProfiles", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  const data = await response.json();
+  console.log(data);
+  if (data) {
+    await loadContent(data);
+  } else {
+    alert("Error loading team data. Please try again later.");
+  }
+});
+
+async function loadContent(data) {
+  // How to format, Make a table or smth, the left will be hte team logo and name, to the right each team member in a colored box
+  // the team captain will always be in the top left corner of the team members, and a different color box, but not say team captain or their role
+  const mainDiv = document.querySelector('.content-container');
+  data.forEach(team => {
+    const teamDiv = document.createElement('div');
+    teamDiv.classList.add('team-container');
+    const logoDiv = document.createElement('div'); 
+    logoDiv.classList.add('team-logo-container');
+    const teamLogo = document.createElement('img');
+    teamLogo.src = `/assets/files/logos/${team.teamName}.png`;
+    teamLogo.alt = `${team.teamName} logo`;
+    teamLogo.classList.add('team-logo');
+    const teamMotto = document.createElement('p');
+    teamMotto.classList.add('team-motto');
+    teamMotto.textContent = team.motto; 
+    const teamMembersDiv = document.createElement('div');
+    teamMembersDiv.classList.add('team-members');
+    //image of team member then name right below it
+    team.teamMembers.forEach((member) => {
+      member = member[0];
+      const memberDiv = document.createElement('div');
+      memberDiv.classList.add('team-member');
+      console.log(member);
+      if (member.role === "Team Captain") {
+        memberDiv.classList.add('captain');
+      } 
+      const memberImage = document.createElement('img');
+      memberImage.src = member.imageURL;
+      memberImage.alt = `${member.firstName} ${member.lastName}`;
+      memberImage.classList.add('member-image');
+      const memberName = document.createElement('p');
+      memberName.textContent = `${member.firstName} ${member.lastName}`;
+      memberDiv.appendChild(memberImage);
+      memberDiv.appendChild(memberName);
+      teamMembersDiv.appendChild(memberDiv);
+    });
+    // no team name
+    logoDiv.appendChild(teamLogo);
+    logoDiv.appendChild(teamMotto);
+    teamDiv.appendChild(logoDiv);
+    const lineBreak = document.createElement('hr');
+    lineBreak.classList.add('team-line-break');
+    teamDiv.appendChild(lineBreak);
+    teamDiv.appendChild(teamMembersDiv);
+    mainDiv.appendChild(teamDiv);
+  });
+  mainDiv.appendChild(teamDiv);
+  
+}
