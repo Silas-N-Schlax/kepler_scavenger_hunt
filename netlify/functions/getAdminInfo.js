69ecb29011114e036806592802bc9800598903f3
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-05-08 06:53:47 -0400

    created server side function to gather all admin info for admin page (PPP  = Password Protected Page)

---
+++ netlify/functions/getAdminInfo.js
@@ -0,0 +1,54 @@
+const database = require("./utils/database.js");
+
+exports.handler = async function(event, context) {
+  const db = await database.returnDatabase();
+ 
+  const teams = await getTeams(db);
+  const players = await getPlayers(db);
+  const winners = await getWinners(db);
+  const accounts = await getAccounts(db);
+  const teamNames = await getTeamNames(db);
+
+
+  if (!teams || !players || !winners || !accounts || !teamNames) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        status: "error",
+        message: "Failed to retrieve data from the database."
+      })
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      status: "success",
+      teams: teams,
+      players: players,
+      winners: winners,
+      accounts: accounts,
+      teamNames: teamNames
+    })
+  }
+}
+
+async function getTeams(db) {
+  return await db.collection("teams").find({}).toArray();
+}
+
+async function getPlayers(db) {
+  return await db.collection("players").find({}).toArray();
+}
+
+async function getWinners(db) {
+  return await db.collection("winners").find({}).toArray();
+}
+
+async function getAccounts(db) {
+  return await db.collection("accounts").find({}).toArray();
+}
+
+async function getTeamNames(db) {
+  const teams = await db.collection("teams").find({}).toArray();
+  return teams.map(team => team.teamName);
+}
