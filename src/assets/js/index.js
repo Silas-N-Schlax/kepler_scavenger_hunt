3048db032649ee343e07cb11bb246c1b52913fbf
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-05-09 15:08:40 -0400

    added index script to check active teams and load them on home page

---
+++ src/assets/js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", async function () { 
+  const response = await fetch('/.netlify/functions/getActiveTeams', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const data = await response.json();
+  if (data.status === "success") {
+    const teamsDiv = document.querySelector(".content-container");
+    data.teams.forEach((teams) => {
+      const team = document.createElement("li");
+      const teamLink = document.createElement("a");
+      teamLink.href = `/content/${teams}/home`;
+      teamLink.textContent = `Team ${teams}`;
+      teamLink.className = "team-link";
+      team.className = "team"
+      team.appendChild(teamLink);
+      teamsDiv.appendChild(team);
+    })
+    console.log(data.teams);
+  } else {
+    alert("Failed to load active teams data.");
+  }
+});
