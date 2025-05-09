406562b3333c8c248dc238a8821251259f82d5a8
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-05-09 15:04:42 -0400

    created backend function to get active teams for display, and locking pages up

---
+++ netlify/functions/getActiveTeams.js
@@ -0,0 +1,32 @@
+const database = require("./utils/database.js");
+
+exports.handler = async function(event, context) {
+  const db = await database.returnDatabase();
+  
+  const teams = await db.collection("teams").find({}).toArray();
+  const activeTeams = []
+  teams.forEach((team) => {
+    if (team.active) {
+      activeTeams.push(team.teamName);
+    }
+  })
+  activeTeams.sort((a, b) => a.localeCompare(b));
+  console.log(activeTeams);
+  
+  if (!teams) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        status: "error",
+        message: "Failed to retrieve data from the database."
+      })
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      status: "success",
+      teams: activeTeams
+    })
+  }
+};
