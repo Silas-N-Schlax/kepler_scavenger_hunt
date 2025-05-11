53d7b43ec18d1309e5e18c1803408535101aef0e
SilasNS <107065641+silasSN@users.noreply.github.com>
2025-05-11 14:46:15 -0400

    Fixed error when if you had data for one team it let you access all teams 🤦‍♂️

--- netlify/functions/checkAccess.js
+++ netlify/functions/checkAccess.js
@@ -3,7 +3,7 @@ const database = require("./utils/database.js");
 exports.handler = async function(event, context) {
   const db = await database.returnDatabase();
   const body = JSON.parse(event.body);
-  if (!body.teamId || !body.key || !body.token || !body.teamName) {
+  if (!body.teamId || !body.key || !body.token || !body.teamName || !body.page) {
     return {
       statusCode: 400,
       body: JSON.stringify({ status: "error", message: "Missing parameters" }),
@@ -13,14 +13,18 @@ exports.handler = async function(event, context) {
   const key = body.key;
   const token = body.token;
   const teamName = body.teamName;
+  const page = body.page;
+  let pageName = page.split("/")[2].split("%20")[1]
+  console.log(teamName === pageName);
 
   const collection = await db.collection("teams").findOne({ teamId: teamId, key: key, token: token, teamName: teamName });
+  console.log(collection);
   if (!collection) {
     return {
       statusCode: 401,
       body: JSON.stringify({ status: "error", message: "Unauthorized" }),
     };
-  } else if (collection.teamId !== teamId || collection.key !== key || collection.token !== token || collection.teamName !== teamName) {
+  } else if (collection.teamId !== teamId || collection.key !== key || collection.token !== token || collection.teamName !== teamName || teamName !== pageName) {
     return {
       statusCode: 401,
       body: JSON.stringify({ status: "error", message: "Unauthorized" }),
